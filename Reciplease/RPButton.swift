0ad80b1c8e8d7c8a5ecc316c2479c9b57d9fb171
anthonymfscott <anthonymfscott@gmail.com>
2020-10-21 13:30:19 +0200

    Add RPButton + RPTextField

---
+++ Reciplease/RPButton.swift
@@ -0,0 +1,35 @@
+//
+//  RPButton.swift
+//  Reciplease
+//
+//  Created by anthonymfscott on 21/10/2020.
+//
+
+import UIKit
+
+class RPButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    init(backgroundColor: UIColor, title: String, font: UIFont.TextStyle) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        setTitle(title, for: .normal)
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: font)
+        configure()
+    }
+
+    private func configure() {
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        layer.cornerRadius = 5
+        titleLabel?.textColor = .white
+    }
+}
---
+++ Reciplease/RPTextField.swift
@@ -0,0 +1,39 @@
+//
+//  RPTextField.swift
+//  Reciplease
+//
+//  Created by anthonymfscott on 21/10/2020.
+//
+
+import UIKit
+
+class RPTextField: UITextField {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func configure() {
+        translatesAutoresizingMaskIntoConstraints = false
+
+        layer.cornerRadius = 10
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.systemGray4.cgColor
+
+        textColor = .label
+        tintColor = .label
+        textAlignment = .left
+        font = UIFont.preferredFont(forTextStyle: .title2)
+
+        backgroundColor = .tertiarySystemBackground
+
+        autocorrectionType = .no
+
+        placeholder = "Lemon, Cheese, Sausages, ..."
+    }
+}
