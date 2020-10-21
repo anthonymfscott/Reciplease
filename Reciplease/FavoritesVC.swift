db86f066284fb9efb486e342f1a7ba98504aa955
anthonymfscott <anthonymfscott@gmail.com>
2020-10-21 12:41:53 +0200

    Delete Storyboard Delete initial ViewController Add SearchVC + FavoritesVC Update SceneDelegate: create UITabBar with UINavigationControllers

---
+++ Reciplease/FavoritesVC.swift
@@ -0,0 +1,29 @@
+//
+//  FavoritesVC.swift
+//  Reciplease
+//
+//  Created by anthonymfscott on 21/10/2020.
+//
+
+import UIKit
+
+class FavoritesVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemGreen
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Reciplease/SearchVC.swift
@@ -0,0 +1,29 @@
+//
+//  SearchVC.swift
+//  Reciplease
+//
+//  Created by anthonymfscott on 21/10/2020.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemPink
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
